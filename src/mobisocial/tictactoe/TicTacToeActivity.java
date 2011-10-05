4bfac16586ffacf8d4f3b67b7c2b07bbe84c5b4c
Ben Dodson <bjdodson@gmail.com>
2011-10-04 23:11:30 -0700

    updates for new api

--- src/mobisocial/tictactoe/TicTacToeActivity.java
+++ src/mobisocial/tictactoe/TicTacToeActivity.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import mobisocial.socialkit.musubi.Musubi;
-import mobisocial.socialkit.musubi.Musubi.Multiplayer;
 import mobisocial.socialkit.musubi.Musubi.StateObserver;
+import mobisocial.socialkit.musubi.multiplayer.RoundRobinMultiplayer;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -22,11 +22,13 @@ import android.widget.Toast;
 public class TicTacToeActivity extends Activity {
     private static final String TAG = "ttt";
 
-    Musubi mMusubi;
-    private Board mBoard;
     private String mToken;
+    private RoundRobinMultiplayer mMultiplayer;
+
     private Button mTokenButton;
-    private Multiplayer mMultiplayer;
+    private final List<Button> mmSquares = new ArrayList<Button>();
+
+    private final String BLANK = "  ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,131 +41,134 @@ public class TicTacToeActivity extends Activity {
             finish();
             return;
         }
-        // All app code is in Board.
-        mMusubi = Musubi.getInstance(this, getIntent());
-        mMultiplayer = new Multiplayer(this, getIntent());
+
+        mMultiplayer = new RoundRobinMultiplayer(this, getIntent());
         mMultiplayer.setStateObserver(mStateObserver);
 
-        mToken = (mMultiplayer.getLocalMemberIndex() == 0) ? "X" : "O";
+        // Bind UI to actions:
+        mmSquares.add((Button)findViewById(R.id.s0));
+        mmSquares.add((Button)findViewById(R.id.s1));
+        mmSquares.add((Button)findViewById(R.id.s2));
+        mmSquares.add((Button)findViewById(R.id.s3));
+        mmSquares.add((Button)findViewById(R.id.s4));
+        mmSquares.add((Button)findViewById(R.id.s5));
+        mmSquares.add((Button)findViewById(R.id.s6));
+        mmSquares.add((Button)findViewById(R.id.s7));
+        mmSquares.add((Button)findViewById(R.id.s8));
+        for (int i = 0; i < 9; i++) {
+            mmSquares.get(i).setOnClickListener(mBoardClickedListener);
+            mmSquares.get(i).setTag(R.id.s0, i);
+        }
+        ((Button)findViewById(R.id.clear)).setOnClickListener(mClearAll);
 
-        mBoard = new Board();        
-        mBoard.render(mMusubi.getFeed().getLatestState());
+        mToken = (mMultiplayer.getLocalMemberIndex() == 0) ? "X" : "O";
+        render(mMultiplayer.getLatestState());
     }
 
-    class Board implements View.OnClickListener {
-        private final List<Button> mmSquares = new ArrayList<Button>();
+    /**
+     * Extracts the board state from the given object
+     * and renders it to screen.
+     */
+    private void render(JSONObject state) {
+        mTokenButton.setText(mToken);
 
-        public Board() {
-            // TODO: It is more efficient to bind each individual
-            // view object to the SocialKit. Can just give root view?
-            mmSquares.add((Button)findViewById(R.id.s0));
-            mmSquares.add((Button)findViewById(R.id.s1));
-            mmSquares.add((Button)findViewById(R.id.s2));
-            mmSquares.add((Button)findViewById(R.id.s3));
-            mmSquares.add((Button)findViewById(R.id.s4));
-            mmSquares.add((Button)findViewById(R.id.s5));
-            mmSquares.add((Button)findViewById(R.id.s6));
-            mmSquares.add((Button)findViewById(R.id.s7));
-            mmSquares.add((Button)findViewById(R.id.s8));
-            for (int i = 0; i < 9; i++) {
-                mmSquares.get(i).setOnClickListener(this);
-                mmSquares.get(i).setTag(R.id.s0, i);
-            }
-            ((Button)findViewById(R.id.clear)).setOnClickListener(mClearAll);
+        String status = "I am player " + mMultiplayer.getLocalMemberIndex()
+                + " and its " + mMultiplayer.getGlobalMemberCursor() + "s turn.";
+        ((TextView)findViewById(R.id.status)).setText(status);
+
+        if (state == null || !state.has("s")) {
+            clearBoard();
+            return; // empty board initialized.
         }
 
-        private synchronized void render(JSONObject state) {
-            mTokenButton.setText(mToken);
+        JSONArray s = state.optJSONArray("s");
+        for (int i = 0; i < 9; i++) {
+            mmSquares.get(i).setText(s.optString(i));
+        }            
+    }
 
-            String status = "I am player " + mMultiplayer.getLocalMemberIndex()
-                    + " and its " + mMultiplayer.getGlobalMemberCursor() + "s turn.";
-            ((TextView)findViewById(R.id.status)).setText(status);
-
-            if (state == null || !state.has("s")) {
-                return; // empty board initialized.
+    /**
+     * Computes the local view of the application state,
+     * which is stored within the UI itself.
+     */
+    private JSONObject getLocalApplicationState() {
+        JSONObject o = new JSONObject();
+        JSONArray s = new JSONArray();
+        try {
+            for (Button b : mmSquares) {
+                s.put(b.getText());
             }
-
-            JSONArray s = state.optJSONArray("s");
-            for (int i = 0; i < 9; i++) {
-                mmSquares.get(i).setText(s.optString(i));
-            }            
+            o.put("s", s);
+        } catch (JSONException e) {
+            Log.wtf(TAG, "Failed to get board state", e);
         }
+        return o;
+    }
 
-        private JSONObject getApplicationState() {
-            JSONObject o = new JSONObject();
-            JSONArray s = new JSONArray();
-            try {
-                for (Button b : mmSquares) {
-                    s.put(b.getText());
-                }
-                o.put("s", s);
-            } catch (JSONException e) {
-                Log.wtf(TAG, "Failed to get board state", e);
-            }
-            return o;
-        }
-
+    private View.OnClickListener mBoardClickedListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
+            // This call isn't necessary, since takeTurn() checks internally.
+            // But it seems like a good thing to do.
             if (!mMultiplayer.isMyTurn()) {
                 return;
             }
 
-            mmSquares.get((Integer)v.getTag(R.id.s0)).setText(mToken);
-            pushUpdate();
-        }
-
-        public void onClickClear() {
-            for (int i = 0; i < 9; i++) {
-                mmSquares.get(i).setText("  ");
+            Button square = mmSquares.get((Integer)v.getTag(R.id.s0));
+            if (!square.getText().equals(BLANK)) {
+                return;
             }
-            pushUpdate();
-        }
 
-        public void pushUpdate() {
-            //mDungBeetle.getFeed().setApplicationState(getApplicationState(), getSnapshotText());
-            mMultiplayer.takeTurn(getApplicationState(), getSnapshotHtml());
+            square.setText(mToken);
+            mMultiplayer.takeTurn(getLocalApplicationState(), getSnapshotHtml());
         }
+    };
 
-        public String getSnapshotText() {
-            StringBuilder snapshot = new StringBuilder();
-            snapshot.append(" " + mmSquares.get(0).getText() +
-                    " | " + mmSquares.get(1).getText() + " | " + mmSquares.get(2).getText());
-            snapshot.append("\n------------\n");
-            snapshot.append(" " + mmSquares.get(3).getText() +
-                    " | " + mmSquares.get(4).getText() + " | " + mmSquares.get(5).getText());
-            snapshot.append("\n------------\n");
-            snapshot.append(" " + mmSquares.get(6).getText() +
-                    " | " + mmSquares.get(7).getText() + " | " + mmSquares.get(8).getText());
-            return snapshot.toString();
-        }
 
-        public String getSnapshotHtml() {
-            StringBuilder html = new StringBuilder("<html><head><style>");
-            html.append("td { border:1px solid black; min-width:18px; }");
-            html.append("table { background-color:#FC6; padding:8px;}");
-            html.append("</style></head>");
-            html.append("<body><div><table><tr>");
-            html.append("<td>&nbsp;").append(mmSquares.get(0).getText()).append("</td>");
-            html.append("<td>&nbsp;").append(mmSquares.get(1).getText()).append("</td>");
-            html.append("<td>&nbsp;").append(mmSquares.get(2).getText()).append("</td>");
-            html.append("</tr><tr>");
-            html.append("<td>&nbsp;").append(mmSquares.get(3).getText()).append("</td>");
-            html.append("<td>&nbsp;").append(mmSquares.get(4).getText()).append("</td>");
-            html.append("<td>&nbsp;").append(mmSquares.get(5).getText()).append("</td>");
-            html.append("</tr><tr>");
-            html.append("<td>&nbsp;").append(mmSquares.get(6).getText()).append("</td>");
-            html.append("<td>&nbsp;").append(mmSquares.get(7).getText()).append("</td>");
-            html.append("<td>&nbsp;").append(mmSquares.get(8).getText()).append("</td>");
-            html.append("</tr></table></body></div></html>");
-            return html.toString();
+    public void clearBoard() {
+        for (int i = 0; i < 9; i++) {
+            mmSquares.get(i).setText(BLANK);
         }
+    }
+
+    public String getSnapshotText() {
+        StringBuilder snapshot = new StringBuilder();
+        snapshot.append(" " + mmSquares.get(0).getText() +
+                " | " + mmSquares.get(1).getText() + " | " + mmSquares.get(2).getText());
+        snapshot.append("\n------------\n");
+        snapshot.append(" " + mmSquares.get(3).getText() +
+                " | " + mmSquares.get(4).getText() + " | " + mmSquares.get(5).getText());
+        snapshot.append("\n------------\n");
+        snapshot.append(" " + mmSquares.get(6).getText() +
+                " | " + mmSquares.get(7).getText() + " | " + mmSquares.get(8).getText());
+        return snapshot.toString();
+    }
+
+    public String getSnapshotHtml() {
+        StringBuilder html = new StringBuilder("<html><head><style>");
+        html.append("td { border:1px solid black; min-width:18px; }");
+        html.append("table { background-color:#FC6; padding:8px;}");
+        html.append("</style></head>");
+        html.append("<body><div><table><tr>");
+        html.append("<td>&nbsp;").append(mmSquares.get(0).getText()).append("</td>");
+        html.append("<td>&nbsp;").append(mmSquares.get(1).getText()).append("</td>");
+        html.append("<td>&nbsp;").append(mmSquares.get(2).getText()).append("</td>");
+        html.append("</tr><tr>");
+        html.append("<td>&nbsp;").append(mmSquares.get(3).getText()).append("</td>");
+        html.append("<td>&nbsp;").append(mmSquares.get(4).getText()).append("</td>");
+        html.append("<td>&nbsp;").append(mmSquares.get(5).getText()).append("</td>");
+        html.append("</tr><tr>");
+        html.append("<td>&nbsp;").append(mmSquares.get(6).getText()).append("</td>");
+        html.append("<td>&nbsp;").append(mmSquares.get(7).getText()).append("</td>");
+        html.append("<td>&nbsp;").append(mmSquares.get(8).getText()).append("</td>");
+        html.append("</tr></table></body></div></html>");
+        return html.toString();
     }
 
     private StateObserver mStateObserver = new StateObserver() {
         @Override
         public void onUpdate(JSONObject state) {
-            mBoard.render(state);
+            render(state);
         }
     };
 
@@ -180,7 +185,10 @@ public class TicTacToeActivity extends Activity {
     private View.OnClickListener mClearAll = new View.OnClickListener() {
         @Override
         public void onClick(View arg0) {
-            mBoard.onClickClear();
+            if (mMultiplayer.isMyTurn()) {
+                clearBoard();
+                mMultiplayer.takeTurn(getLocalApplicationState(), getSnapshotHtml());
+            }
         }
     };
 }
